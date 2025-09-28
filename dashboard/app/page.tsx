85639c4c2696c48433d306b31edbb47064db8c98
Trevor Bedson <trevobedson@gmail.com>
2025-09-28 13:32:34 -0400

    remove web dashboard; Use grafana for visualization

--- dashboard/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { auth, signIn, signOut } from "@/auth";
-import { Dashboard } from "@/components/dashboard";
-
-export default async function Home() {
-  const session = await auth()
-  if(!session) return signIn()
-  return <Dashboard />
-}
